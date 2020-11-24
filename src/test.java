cc6198625375aafb6d4f15b4338a29a755984153
gokcen keskin <keskin.gokcen@gmail.com>
2020-11-24 19:34:25 +0000

    sepete ekleme yapildi

---
+++ src/test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("1.islem");
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("yeni metodum hazir");
+        System.out.println("Sepete ekle kismlari yapildi");
+    }
+}
