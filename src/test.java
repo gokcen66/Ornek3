ebfe1df838973bfd9d642966df04ef2849ab61ea
gokcen keskin <keskin.gokcen@gmail.com>
2020-11-24 19:43:14 +0000

    sepetten sil yapildi sepete ekle fixlendi(noktali sayi duzeltme)

--- src/test.java
+++ src/test.java
@@ -3,10 +3,13 @@ public class test {
         System.out.println("1.islem");
 
 
+    }public  static void yeniMetod()
+    {
+        System.out.println("yeni metodum hazir");
     }
 
-    public static void main(String[] args) {
-        System.out.println("yeni metodum hazir");
+    public static void SepettenSil() {
+
         System.out.println("Sepete ekle kismlari yapildi");
     }
 }
